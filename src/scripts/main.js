d516d99879ddfa5b42efbb98470917c3ae0f6838
Aaron Deimund <acdeimund@gmail.com>
2025-01-01 15:14:38 -0600

    Learning Vite

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+import '../css/style.css'
+import javascriptLogo from '../assets/javascript.svg'
+import viteLogo from '../assets/vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ export default {
     root: 'src',
     build: {
       outDir: '../docs',
-      emptyOutDir: true, // also necessary
+      emptyOutDir: true,
     }
   }
